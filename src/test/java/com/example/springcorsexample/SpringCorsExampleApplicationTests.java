809d9ad95677469daf911252e733cfbd893d6a47
Luana da Rosa <lrosa.silva@outlook.com>
2021-11-24 15:50:38 -0300

    cors example completed

---
+++ src/test/java/com/example/springcorsexample/SpringCorsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcorsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
